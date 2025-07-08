a37cf2fd0e03e2116b7eb8fbccaebaf398ec156d
sohee00314 <sohee00314@gmail.com>
2025-07-08 22:31:20 +0900

    🎉 Start : 강의1차(프로젝트 시작, 환경설정)

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,5 +12,37 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "Hello Spring!");
         return "hello";
+    }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model){
+        //"name" => key
+        model.addAttribute("name", name);
+        return "hello-mvc";
+    }
+    //hello-spring?name=*** 이면 name 위치에 ***이 출력
+    @GetMapping("hello-spring")
+    @ResponseBody
+    public String helloSpring(@RequestParam(value = "name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello"+name;
+    }
+    @GetMapping("hello-api")
+    @ResponseBody
+    //객체를 넘기면 json 반환
+    public Hello helloApi(@RequestParam(value = "name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello{
+        private String name;
+        //java 빈 표준 방식(private은 접근할 수 있도록 하는 것)
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
