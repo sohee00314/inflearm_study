b4480384791ae3f50a843c2e5be80c2e8fa69b11
sohee00314 <sohee00314@gmail.com>
2025-07-14 01:51:28 +0900

    ➕ Dependency : 강의5-2(직접 스프링빈 등록)

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -2,8 +2,6 @@ package hello.hello_spring.service;
 
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
